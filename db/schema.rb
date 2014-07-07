c2008671ba5543a97ee34fd8a81ea294531b068d
Merey Zholdas <merey.90@gmail.com>
2014-07-07 15:00:41 +0000

    4,5,6 glava

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140702153200) do
+ActiveRecord::Schema.define(version: 20140707145322) do
 
   create_table "posts", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "password_digest"
   end
 
 end
