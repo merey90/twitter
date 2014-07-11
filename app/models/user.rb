6036a623a39f7ada68d43628d37ebe6feb61a375
Merey Zholdas <merey.90@gmail.com>
2014-07-11 14:54:18 +0000

    added micropost feature

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  
   before_save { self.email = email.downcase }
   before_create :create_remember_token
   
@@ -9,6 +10,8 @@ class User < ActiveRecord::Base
   validates :password, length: { minimum: 6 }
   has_secure_password
   
+  has_many :microposts, dependent: :destroy
+  
   before_save { self.email = email.downcase }
   
   def User.new_remember_token
@@ -17,6 +20,11 @@ class User < ActiveRecord::Base
 
   def User.encrypt(token)
     Digest::SHA1.hexdigest(token.to_s)
+  end
+  
+  def feed
+    # Это предварительное решение. См. полную реализацию в "Following users".
+    Micropost.where("user_id = ?", id)
   end
 
   private
